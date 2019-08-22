49e8784676c28eadd0c6b4deb348a833468bbf4d
Justin Miller <jayman1294@gmail.com>
2019-08-22 18:18:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,7 +16,7 @@ def introduction_with_language(name, language)
 end
 
 introduction_with_language("Dan", "Ember.js")
-introduction_with_language("Josh ", "React")
+introduction_with_language("Josh", "React")
 
 
 def introduction_with_language_optional(name, language = "Ruby")
