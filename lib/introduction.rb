37ec56fe1d650108864cc383dd0851a729a13c82
Justin Miller <jayman1294@gmail.com>
2019-08-22 18:16:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -19,7 +19,7 @@ introduction_with_language("Dan", "Ember.js")
 introduction_with_language("Josh ", "React")
 
 
-def introduction_with_language(name = "Ruby", language)
+def introduction_with_language(name, language = )
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
